5d6f017ad722d4a9d07851c8d53680a044f25185
mono <mono@monoweb.info>
2015-04-14 22:16:42 +0900

    Problem 6

---
+++ Problem0006.java
@@ -0,0 +1,9 @@
+public class Problem0006 {
+    public static void main(String[] args) {
+        final long n = 100;
+        // divided by (n * (n + 1) / 2)
+        final long sumOfSquares = (2 * n + 1) / 3;
+        final long squareOfSum = n * (n + 1) / 2;
+        System.out.println((squareOfSum - sumOfSquares) * n * (n + 1) / 2);
+    }
+}
