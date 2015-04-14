87e3d3feebfad504066ab3d83f9aea8eb6bc54f9
mono <mono@monoweb.info>
2015-04-14 21:28:47 +0900

    Problem 3

---
+++ Problem0003.java
@@ -0,0 +1,19 @@
+public class Problem0003 {
+    public static void main(String[] args) {
+        final long target = 600_851_475_143l;
+        final long sqrtTarget = (long)Math.sqrt(target);
+
+        long primeFactor = 0;
+
+        long x = target;
+        for (long i = 2; i <= sqrtTarget; ++i) {
+            if (x % i == 0) {
+                x /= i;
+                primeFactor = i;
+            }
+        }
+
+        System.out.println(primeFactor);
+    }
+}
+
