0562ae45bc9532060ccb204a271aa391752898d2
mono <mono@monoweb.info>
2015-04-16 13:03:26 +0900

    Problem 10

---
+++ Problem0010.java
@@ -0,0 +1,21 @@
+public class Problem0010 {
+    public static void main(String[] args) {
+        final int max = 2_000_000 - 1;
+
+        final boolean[] table = new boolean[max + 1];
+        table[1] = true;
+
+        long sum = 0;
+        for (int i = 1; i <= max; ++i) {
+            if (table[i]) {
+                continue;
+            }
+            for (int j = i * 2; j <= max; j += i) {
+                table[j] = true;
+            }
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
+
