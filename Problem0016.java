2ca7a5397cd9e4b69c22961fb16f77f348fa7471
mono <mono@monoweb.info>
2015-04-18 01:27:59 +0900

    Problem 16

---
+++ Problem0016.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+
+public class Problem0016 {
+    public static void main(String[] args) {
+        final String str = (new BigInteger("2")).pow(1000).toString();
+
+        int result = 0;
+        for (int i = 0, n = str.length(); i < n; ++i) {
+            result += str.charAt(i) - '0';
+        }
+        System.out.println(result);
+    }
+}
+
