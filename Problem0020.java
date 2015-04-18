6906d7dd0cc66ee9b3dc699d684c2b47084f1299
mono <mono@monoweb.info>
2015-04-18 13:24:43 +0900

    Problem 20

---
+++ Problem0020.java
@@ -0,0 +1,19 @@
+import java.math.BigInteger;
+
+public class Problem0020 {
+    public static void main(String[] args) {
+        final int n = 100;
+        BigInteger fact = new BigInteger("1");
+        for (int i = 2; i <= n; ++i) {
+            fact = fact.multiply(new BigInteger(Integer.toString(i)));
+        }
+
+        final String str = fact.toString();
+
+        int result = 0;
+        for (int i = 0, m = str.length(); i < m; ++i) {
+            result += str.charAt(i) - '0';
+        }
+        System.out.println(result);
+    }
+}
