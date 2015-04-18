a6fd29d6e6c2efbee221b3f0b3bb6f2108dec585
mono <mono@monoweb.info>
2015-04-18 13:20:39 +0900

    Problem 19

---
+++ Problem0019.java
@@ -0,0 +1,16 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class Problem0019 {
+    public static void main(String[] args) {
+        int result = 0;
+        LocalDate date = LocalDate.of(1901, 1, 1);
+        for (int i = 0, n = 100 * 12; i < n; ++i) {
+            if (date.getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
+                ++result;
+            }
+            date = date.plusMonths(1);
+        }
+        System.out.println(result);
+    }
+}
